b8b7b77993a1410d35df615cea15bd7db0c69b69
Rune Hoeper <hoeper@johnson.itb.biologie.hu-berlin.de>
2025-03-12 13:05:32 +0100

    refine simulations and polish notebook

--- smallmodel/simulate.py
+++ smallmodel/simulate.py
@@ -2,9 +2,10 @@ import roadrunner
 import matplotlib.pyplot as plt
 import numpy as np
 import multiprocessing as mp
+from typing import List
 
 
-def get_steady_state(fax):
+def get_steady_state(fax: float) -> List[float]:
     rr.reset()
     rr.M_FAx = fax
     rr.steadyStateSelections = ["M_LD", "M_TAG", "M_FA"]
@@ -18,9 +19,6 @@ rr.simulate(
     selections=["time", "M_LD", "M_TAG", "M_FA"]
 )
 fig, ax = plt.subplots()
-# ax.plot(rr.time, rr.M_LD, label="LD")
-# ax.plot(rr.time, rr.M_FAx, label="FAx")
-# ax.legend()
 rr.plot(show=False)
 ax.legend()
 ax.set_xlabel("Time")
@@ -29,30 +27,21 @@ fig.tight_layout()
 
 fig.savefig("SimpleFALiver.png")
 
+fax_range = np.arange(0, 5, .01)
 result_dict = {
     "M_FAx": list(),
     "M_LD": list(),
     "M_TAG": list(),
     "M_FA": list()
 }
-# for fax in np.arange(0, 5, .01):
-#     rr.reset()
-#     rr.M_FAx = fax
-#     rr.steadyStateSelections = ["M_LD", "M_TAG", "M_FA"]
-#     res = rr.getSteadyStateValues()
-
-#     result_dict["M_FAx"].append(fax)
-#     result_dict["M_LD"].append(res[0])
-#     result_dict["M_TAG"].append(res[1])
-#     result_dict["M_FA"].append(res[2])
 
 with mp.Pool(mp.cpu_count()) as pool:
     results = pool.map(
         get_steady_state,
-        np.arange(0, 5, .01)
+        fax_range
     )
 
-for fax, res in zip(np.arange(0, 5, .01), results):
+for fax, res in zip(fax_range, results):
     result_dict["M_FAx"].append(fax)
     result_dict["M_LD"].append(res[0])
     result_dict["M_TAG"].append(res[1])
